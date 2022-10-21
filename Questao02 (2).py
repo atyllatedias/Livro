d5a068d13daa191bb5ae44abfc80b74db7b0aa98
atyllatedias <113216392+atyllatedias@users.noreply.github.com>
2022-10-20 22:17:49 -0300

    Add files via upload

---
+++ Questao02 (2).py
@@ -0,0 +1,23 @@
+ingrs: float 
+qntingrs: int 
+desp: int 
+renda: float 
+lucro: float
+
+import os
+
+ingrs = 5
+desp = 200
+qntingrs = 120
+
+os.system("cls")
+
+while ingrs >= 1:
+    renda = ingrs * qntingrs
+    lucro = renda - desp
+    print(f"|O preço do ingresso é: \t\t    {ingrs:.1f} |")
+    print(f"|A quantidade de ingressos vendidos é: \t    {qntingrs} |") 
+    print(f"|O lucro com essa quantidade de ingressos é: {lucro:.1f}|")
+    print("=" *49)
+    qntingrs += 26
+    ingrs -= 0.5
